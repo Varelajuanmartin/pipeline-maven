64031b62b180bf86debba2073237496d3a8e253b
varelajuanmartin <varelajuanmartin@gmail.com>
2023-03-05 00:47:48 -0300

    Fixed workspace path

--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -4,4 +4,6 @@ echo "**********************************"
 echo "******** TESTING THE CODE ********"
 echo "**********************************"
 
-docker run --rm -v $PWD/java-app:/app -v /root/.m2/:/root/.m2 -w /app maven:3.8.6-eclipse-temurin-11-alpine "$@"
+WORKSPACE=/home/juan/JenkinsFromZero/JenkinsFromZero/jenkins_home/workspace/pipeline-docker-maven
+
+docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2 -w /app maven:3.8.6-eclipse-temurin-11-alpine "$@"
